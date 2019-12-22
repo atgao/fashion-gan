629fadcab7f9f68743770b5dfe3f3182f38a055d
atgao <alice.gao@princeton.edu>
2019-12-21 19:09:44 -0500

    fixed a little bit

--- aae.py
+++ aae.py
@@ -7,6 +7,7 @@ import itertools
 import torchvision.transforms as transforms
 from torchvision.utils import save_image
 from config import *
+from utils import *
 
 from torch.utils.data import DataLoader
 from torchvision import datasets
@@ -20,15 +21,15 @@ import torch
 os.makedirs("images", exist_ok=True)
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--n_epochs", type=int, default=200, help="number of epochs of training")
-parser.add_argument("--batch_size", type=int, default=64, help="size of the batches")
-parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")
+# parser.add_argument("--n_epochs", type=int, default=200, help="number of epochs of training")
+# parser.add_argument("--batch_size", type=int, default=64, help="size of the batches")
+# parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")
 parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
 parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
 parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
 parser.add_argument("--latent_dim", type=int, default=10, help="dimensionality of the latent code")
 parser.add_argument("--img_size", type=int, default=32, help="size of each image dimension")
-parser.add_argument("--channels", type=int, default=1, help="number of image channels")
+# parser.add_argument("--channels", type=int, default=1, help="number of image channels")
 parser.add_argument("--sample_interval", type=int, default=400, help="interval between image sampling")
 opt = parser.parse_args()
 print(opt)
@@ -143,15 +144,15 @@ dataloader = torch.utils.data.DataLoader(
         type="train", 
         transform=data_transform_train
     ),
-    batch_size=opt.batch_size,
+    batch_size=TRAIN_BATCH_SIZE,
     shuffle=True,
 )
 
 # Optimizers
 optimizer_G = torch.optim.Adam(
-    itertools.chain(encoder.parameters(), decoder.parameters()), lr=opt.lr, betas=(opt.b1, opt.b2)
+    itertools.chain(encoder.parameters(), decoder.parameters()), lr=LR, betas=(opt.b1, opt.b2)
 )
-optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
+optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=LR, betas=(opt.b1, opt.b2))
 
 Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
 
@@ -168,7 +169,7 @@ def sample_image(n_row, batches_done):
 #  Training
 # ----------
 
-for epoch in range(opt.n_epochs):
+for epoch in range(N_EPOCHS):
     for i, (imgs, _) in enumerate(dataloader):
 
         # Adversarial ground truths
@@ -213,9 +214,17 @@ for epoch in range(opt.n_epochs):
 
         print(
             "[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f]"
-            % (epoch, opt.n_epochs, i, len(dataloader), d_loss.item(), g_loss.item())
+            % (epoch, N_EPOCHS, i, len(dataloader), d_loss.item(), g_loss.item())
         )
 
         batches_done = epoch * len(dataloader) + i
         if batches_done % opt.sample_interval == 0:
             sample_image(n_row=10, batches_done=batches_done)
+
+# ----------
+#  Save Model and create Training Log
+# ----------
+print(opt)
+print("Saved Encoder to {}".format(save_model(model, epoch, "encoder")))
+print("Saved Decoder to {}".format(save_model(model, epoch, "decoder")))
+print("Saved Discriminator to {}".format(save_model(model, epoch, "discriminator")))
---
+++ utils.py
@@ -0,0 +1,17 @@
+import os
+import time
+import torch
+import numpy as np
+import torch.nn as nn
+from torchvision import transforms
+import torch.nn.functional as F
+
+
+def save_model(model, epoch, model_type):
+	os.makedirs("model", exist_ok=True)
+	save_path = "model/{}_{}.pkl".format(model_type,epoch)
+	torch.save(model.state_dict(), save_path)
+	return save_path
+
+def load_model:
+	pass 
