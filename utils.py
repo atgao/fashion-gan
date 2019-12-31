098b5d8005da9c47072f86a7404e296ada6df6f1
Unknown <junehopark98@gmail.com>
2019-12-30 17:32:14 -0800

    Added learning rate to naming scheme

--- utils.py
+++ utils.py
@@ -7,9 +7,9 @@ from torchvision import transforms
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
 
-def save_model(model, epoch, model_type, categories, batch_size, date):
+def save_model(model, epoch, model_type, categories, lr, batch_size, date):
 	os.makedirs("model", exist_ok=True)
-	save_path = "model/{}.pkl".format(gen_name(model_type, epoch, categories, batch_size, date))
+	save_path = "model/{}.pkl".format(gen_name(model_type, epoch, categories, lr, batch_size, date))
 	torch.save(model.state_dict(), save_path)
 	return save_path
 
@@ -17,7 +17,7 @@ def load_model(model_type, epoch, categories, batch_size, date):
 	model_path = "model/{}.pkl".format(gen_name(model_type, epoch, categories, batch_size, date))
 	return torch.load(model_path)
 
-def plot_losses(model_name, G_losses, D_losses, categories, batch_size, epoch, date, show=True, save=True):
+def plot_losses(model_name, G_losses, D_losses, categories, lr, batch_size, epoch, date, show=True, save=True):
 	plt.figure(figsize=(10,5))
 	plt.title("Generator and Discriminator Loss During Training")
 	plt.plot(G_losses,label="G")
@@ -27,7 +27,7 @@ def plot_losses(model_name, G_losses, D_losses, categories, batch_size, epoch, d
 	plt.legend()
 	if save:
 		os.makedirs("plots", exist_ok=True)
-		plt.savefig('plots/%s.png' % gen_name(model_name, categories, batch_size, epoch, date))
+		plt.savefig('plots/%s.png' % gen_name(model_name, categories, lr, batch_size, epoch, date))
 
 	if show:
 		plt.show(block=False)
